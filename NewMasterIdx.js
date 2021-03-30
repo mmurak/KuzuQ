403d9b2720e82bc10e4ce96e635547ca017dba6b
Masaaki Murakami <masaaki.murakami@gmail.com>
2021-03-30 20:17:21 +1300

    Release Neo-module
    Unified version.

---
+++ NewMasterIdx.js
@@ -0,0 +1,6 @@
+selectableData = [
+  [ "小1漢字Part.1（40文字）", "./NewMaster001A.js" ],
+  [ "小1漢字Part.2（40文字）", "./NewMaster001B.js" ],
+  [ "小2漢字Part.1（60文字）", "./NewMaster002A.js" ],
+  [ "小2漢字Part.2（50文字）", "./NewMaster002B.js" ],
+];
