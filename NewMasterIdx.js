8091a7845918765a66befa633b9567be4c7ae0b9
Masaaki Murakami <masaaki.murakami@gmail.com>
2021-03-31 22:22:48 +1300

    DataBrowser unification

--- NewMasterIdx.js
+++ NewMasterIdx.js
@@ -3,4 +3,5 @@ selectableData = [
   [ "小1漢字Part.2（40文字）", "./NewMaster001B.js" ],
   [ "小2漢字Part.1（60文字）", "./NewMaster002A.js" ],
   [ "小2漢字Part.2（50文字）", "./NewMaster002B.js" ],
+  [ "小2漢字Part.3（48文字）", "./NewMaster002C.js" ],
 ];
