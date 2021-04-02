92a5a85e8e73192d5fdfc2220d17aec5845f687c
Masaaki Murakami <masaaki.murakami@gmail.com>
2021-04-02 22:25:55 +1300

    Fix index file.

--- NewMasterIdx.js
+++ NewMasterIdx.js
@@ -1,5 +1,4 @@
 selectableData = [
-  [ "テスト（5文字）", "./NewMaster000.js" ],
   [ "小1漢字Part.1（40文字）", "./NewMaster001A.js" ],
   [ "小1漢字Part.2（40文字）", "./NewMaster001B.js" ],
   [ "小2漢字Part.1（60文字）", "./NewMaster002A.js" ],
