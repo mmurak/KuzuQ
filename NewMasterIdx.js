e00d532c0be674d9b8683072af1dbb084850e487
Masaaki Murakami <masaaki.murakami@gmail.com>
2021-04-02 21:32:46 +1300

    小学校3年生（1/4）追加

--- NewMasterIdx.js
+++ NewMasterIdx.js
@@ -1,7 +1,8 @@
 selectableData = [
+  [ "テスト（5文字）", "./NewMaster000.js" ],
   [ "小1漢字Part.1（40文字）", "./NewMaster001A.js" ],
   [ "小1漢字Part.2（40文字）", "./NewMaster001B.js" ],
   [ "小2漢字Part.1（60文字）", "./NewMaster002A.js" ],
   [ "小2漢字Part.2（50文字）", "./NewMaster002B.js" ],
-  [ "小2漢字Part.3（48文字）", "./NewMaster002C.js" ],
+  [ "小3漢字Part.1（50文字）", "./NewMaster003A.js" ],
 ];
